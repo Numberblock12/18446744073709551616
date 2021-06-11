99f5d565bebd9ad07f1695fa5343f24e0295f964
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2021-06-11 11:08:26 -0500

    2^53 8x8

--- js_9007199254740992/html_actuator.js
+++ js_9007199254740992/html_actuator.js
@@ -62,7 +62,33 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  if (tile.value >= Math.pow(10, 11)) {
+    inner.textContent = Math.floor(tile.value / Math.pow(10, 9)) + "B";
+  }
+  if (tile.value >= Math.pow(10, 12)) {
+    inner.textContent = Math.floor(tile.value / Math.pow(10, 12)) + "T";
+  }
+  if (tile.value >= Math.pow(10, 15)) {
+    inner.textContent = Math.floor(tile.value / Math.pow(10, 15)) + "q";
+  }
+  if (tile.value >= Math.pow(10, 18)) {
+    inner.textContent = Math.floor(tile.value / Math.pow(10, 18)) + "Q";
+  }
+  if (tile.value >= Math.pow(10, 21)) {
+    inner.textContent = Math.floor(tile.value / Math.pow(10, 21)) + "s";
+  }
+  if (tile.value >= Math.pow(10, 24)) {
+    inner.textContent = Math.floor(tile.value / Math.pow(10, 24)) + "S";
+  }
+  if (tile.value >= Math.pow(10, 27)) {
+    inner.textContent = Math.floor(tile.value / Math.pow(10, 27)) + "O";
+  }
+  if (tile.value >= Math.pow(10, 30)) {
+    inner.textContent = Math.floor(tile.value / Math.pow(10, 30)) + "N";
+  }
+  if (tile.value < Math.pow(10, 11)) {
+    inner.textContent = tile.value;
+  }
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
